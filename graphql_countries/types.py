2921df4ca3325528463311accae764227846627f
mongkok <domake.io@gmail.com>
2018-01-14 00:23:43 +0530

    Fix Types collision

--- graphql_countries/types.py
+++ graphql_countries/types.py
@@ -12,9 +12,15 @@ class CamelJSON(GenericScalar):
     def serialize(cls, value):
         return dashed_to_camel(value)
 
+    class Meta:
+        name = 'CountriesCamelJSON'
+
 
 class GeoJSON(graphene.Scalar):
 
     @classmethod
     def serialize(cls, value):
         return json.loads(value.geojson)
+
+    class Meta:
+        name = 'CountriesGeoJSON'
