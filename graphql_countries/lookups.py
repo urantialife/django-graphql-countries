1cff8ee6673dfbf79c2d3e205e2be55393e1ea09
mongkok <domake.io@gmail.com>
2018-01-14 00:23:34 +0530

    Clean lookups

--- graphql_countries/lookups.py
+++ graphql_countries/lookups.py
@@ -1,7 +1,8 @@
-TEXT_LOOKUPS = [
-    'exact', 'iexact', 'in', 'contains', 'icontains', 'startswith',
+CHOICES_LOOKUPS = ['exact', 'in']
+
+TEXT_LOOKUPS = CHOICES_LOOKUPS + [
+    'iexact', 'contains', 'icontains', 'startswith',
     'istartswith', 'endswith', 'iendswith', 'regex', 'iregex',
 ]
 
-CHOICES_LOOKUPS = ['exact', 'in']
 RANGE_LOOKUPS = ['exact', 'gt', 'gte', 'lt', 'lte']
